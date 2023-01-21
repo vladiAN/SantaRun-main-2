55f352fe1e6e77e7f71d67796c5303c0ea4ae67d
Andrushok Vlad <andrushok.vlad@gmail.com>
2023-01-21 17:55:17 +0200

    no message

--- SantaRun-main 2/2DGame/MenuScene.swift
+++ SantaRun-main 2/2DGame/MenuScene.swift
@@ -11,26 +11,55 @@ class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+        self.backgroundColor = SKColor(red: 0.61, green: 0.88, blue: 1.00, alpha: 1.00)
         
+        let logo = SKSpriteNode(imageNamed: "Logo1")
         
-        let header = SKSpriteNode(imageNamed: "Logo1")
+        logo.size.height = logo.size.height / 2
+        logo.size.width = logo.size.width / 2
+        logo.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - (logo.frame.height / 2) - 16 )
+        self.addChild(logo)
         
-        header.size.height = header.size.height / 2
-        header.size.width = header.size.width / 2
-        header.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - (header.frame.height / 2) - 16 )
-        self.addChild(header)
+        let button1 = ButtonNode(title: "play", backgroundName: "Button")
+        button1.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        button1.name = "play"
+        button1.label.name = "play"
+        
+        addChild(button1)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let location = touches.first!.location(in: self)
         let node = self.atPoint(location)
         
-        if node.name == "runButton" {
+        if node.name == "play" {
             let tranzition = SKTransition.crossFade(withDuration: 1.0)
             let gameScene = GameScene(size: (self.view?.frame.size)!)
             gameScene.scaleMode = .aspectFill
             self.scene?.view?.presentScene(gameScene, transition: tranzition)
         }
+    }
+}
+
+class ButtonNode: SKSpriteNode {
+    let label: SKLabelNode = {
+        let label = SKLabelNode(text: "text")
+        label.fontColor = UIColor(red: 0.07, green: 0.45, blue: 0.87, alpha: 1.00)
+        label.fontName = "Chalkduster"
+        label.horizontalAlignmentMode = .center
+        label.verticalAlignmentMode = .center
+        label.zPosition = 2
+        return label
+    }()
+    
+    init(title: String, backgroundName: String) {
+        let texture = SKTexture(imageNamed: backgroundName)
+        super.init(texture: texture, color: .clear, size: texture.size())
+        label.text = title.uppercased()
+        addChild(label)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
